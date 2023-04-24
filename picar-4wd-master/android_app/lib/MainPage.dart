fdc7b6c068dc22f2b893b7f24569faae3b4f62a1
Lavanya Puri <lpuri2@illinois.edu>
2023-04-24 16:01:38 -0500

    added android app

---
+++ picar-4wd-master/android_app/lib/MainPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_app/MainPage.dart';
+
+import 'main.dart';
+
+
+void main() => runApp(new ExampleApplication());
+
+class ExampleApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainPage());
+  }
+}
