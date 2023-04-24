f1f845d213034f9a86130cd068d98f317f490d70
Johann Pally <pallyjohann@gmail.com>
2023-04-24 17:38:00 -0500

    testing bluetooth connection

--- picar-4wd-master/main.py
+++ picar-4wd-master/main.py
@@ -7,55 +7,88 @@ from history import History
 HOST = "192.168.50.217" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    disp = Dispense()
-    vis = Vision()
-    snd = Sound()
-    hist = History()
+from bluedot.btcomm import BluetoothServer
+from signal import pause
+
+disp = Dispense()
+vis = Vision()
+snd = Sound()
+hist = History()
+
+def received_handler(data):
+    global disp
+    trts = disp.act() 
+    if trts is not None:
+        mess = f"Nova has been given {trts} treats so far!".encode('ascii')
+        s.send(mess)
+    
+s = BluetoothServer(received_handler)
+
+message= f"alert test".encode('ascii')
+s.send(str(message))
+
+pause()
+
+while False:
+    #VISION
+    det = vis.check_dog()
+
+    #SOUND
+    smp = snd.get_sample()
+
+    #HISTORY
+    hist.add_position(det)
+    hist.add_sample(smp)
+
+    if hist.check_noise():
+        message= f"NOISE alert".encode('ascii')
+        s.send(str(message))
+    if hist.check_spaz():
+        message= f"MOVEMENT alert".encode('ascii')
+        s.send(str(message))
 
 
-    try:
-        while 1:
-            #VISION
-            det = vis.check_dog()
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind((HOST, PORT))
+#     s.listen()
+#     disp = Dispense()
+#     vis = Vision()
+#     snd = Sound()
+#     hist = History()
 
-            #SOUND
-            smp = snd.get_sample()
 
-            #HISTORY
-            hist.add_position(det)
-            hist.add_sample(smp)
-
-            #MESSAGING
-            client, clientInfo = s.accept()
-            print("server recv from: ", clientInfo)
-
-            #DO ALERTS
-            mess = hist.check_noise()
-            if mess is not None:
-                message = f"{mess}".encode('ascii')
-                client.sendall(message)
-
-            mess = hist.check_spaz()
-            if mess is not None:
-                message = f"{mess}".encode('ascii')
-                client.sendall(message)
-
-            data = client.recv(1024) 
-            if data == b'stop':
-                print("Stopping socket")
-                client.close()
-                s.close()
-                break
-            if data != b"":
-                trts = disp.act(data)
-                message= f"Nova has had {trts} treats!".encode('ascii')
-                client.sendall(message) # Echo back to client
+#     try:
+#         while 1:
             
-    except Exception as e:
-        print('error: ', e)
-        print("Closing socket")
-        client.close()
-        s.close()
+
+#             #MESSAGING
+#             client, clientInfo = s.accept()
+#             print("server recv from: ", clientInfo)
+
+#             #DO ALERTS
+#             mess = hist.check_noise()
+#             if mess is not None:
+#                 message = f"{mess}".encode('ascii')
+#                 client.sendall(message)
+
+#             mess = hist.check_spaz()
+#             if mess is not None:
+#                 message = f"{mess}".encode('ascii')
+#                 client.sendall(message)
+
+#             data = client.recv(1024) 
+#             if data == b'stop':
+#                 print("Stopping socket")
+#                 client.close()
+#                 s.close()
+#                 break
+#             if data != b"":
+#                 trts = disp.act(data)
+#                 message= f"Nova has had {trts} treats!".encode('ascii')
+#                 client.sendall(message) # Echo back to client
+            
+#     except Exception as e:
+#         print('error: ', e)
+#         print("Closing socket")
+#         client.close()
+#         s.close()
