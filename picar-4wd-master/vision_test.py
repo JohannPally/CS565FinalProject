2fbd41daf6e407953f19a985800e0ddac36ed369
Johann Pally <pallyjohann@gmail.com>
2023-04-24 15:04:58 -0500

    print test

--- picar-4wd-master/vision_test.py
+++ picar-4wd-master/vision_test.py
@@ -2,5 +2,8 @@ from dog_detection import Vision
 
 vis  = Vision()
 for i in range(5):
-    print(vis.check_dog())
+    det = vis.check_dog()
+    bb = det.bounding_box
+    print(bb.origin_x, bb.origin_y, bb.width, bb.height)
+    
     input()
